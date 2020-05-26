112d5d45d06fd222b44b0bfbdee0b22df8331be6
Anderson Rodriguez Molina <anderson_rodriguez@ucne.edu.do>
2020-05-26 11:45:19 -0400

    Creando carpetas

--- RegistrodePersonas/MainWindow.xaml.cs
+++ RegistrodePersonas/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace RegistrodePersonas
 {
@@ -23,6 +25,27 @@ namespace RegistrodePersonas
         public MainWindow()
         {
             InitializeComponent();
+        }
+       
+    } 
+    public class persona
+    {
+        [Key]
+        public int Id { get; set; }
+        public string nombre { get; set; } 
+        public string telefono { get; set; } 
+        public string cedula { get; set; }
+        public string direccion { get; set; }       
+        public string nacimiento { get; set; }
+
+    }
+
+    public class contecto : DbContext
+    {
+        public DbSet<persona> personas { set; get; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Movies.db");
         }
     }
 }
---
+++ RegistrodePersonas/Migrations/20200526040140_Inicial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistrodePersonas.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "personas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    nombre = table.Column<string>(nullable: true),
+                    telefono = table.Column<string>(nullable: true),
+                    cedula = table.Column<string>(nullable: true),
+                    direccion = table.Column<string>(nullable: true),
+                    nacimiento = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_personas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "personas");
+        }
+    }
+}
